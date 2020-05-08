3e973feaa3bcf8a06cc1cb0a32735a9d45eec46c
Kyle Walker <KyleJamesWalker@gmail.com>
2020-05-08 08:48:11 -0700

    Handle WorkshopFileInfos.json

--- src/tts_tools/libtts.py
+++ src/tts_tools/libtts.py
@@ -175,4 +175,9 @@ def get_save_name(filename):
 
     with open(filename, 'r', encoding='utf-8') as infile:
         save = json.load(infile)
+
+    # Handle possible WorkshopFileInfos.json
+    if isinstance(save, list):
+        return None
+
     return save["SaveName"]
