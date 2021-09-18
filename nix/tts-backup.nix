86a50f8882557fd99744ae79f0063dfb3cca66de
Sebastian Reuße <seb@schattenkopie.de>
2021-09-18 18:39:19 +0200

    build(nix): prefer passing dependencies explicitly

--- nix/tts-backup.nix
+++ nix/tts-backup.nix
@@ -1,7 +1,7 @@
-{ pkgs }:
-pkgs.python38Packages.buildPythonApplication {
+{ python3Packages }:
+python3Packages.buildPythonApplication {
   pname = "tts-backup";
   version = "0.1.0.3";
-  propagatedBuildInputs = (with pkgs.python38Packages; [ setuptools tkinter ]);
+  propagatedBuildInputs = (with python3Packages; [ setuptools tkinter ]);
   src = ../.;
 }
